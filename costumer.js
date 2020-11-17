d18873c27b112f8627d830390c4705a1b747bf52
habibiealaydrus <habibiemohamad@gmail.com>
2020-11-17 17:37:37 +0700

    sya mentok pak....hehehe

--- costumer.js
+++ costumer.js
@@ -1,4 +1,4 @@
-costumer = [
+pelanggan = [
     {id:1, name:'habibie', address:'tanah abang'},
     {id:2, name:'rizki', address:'kuningan timur'},
     {id:3, name:'nindya', address:'setia budi'},
@@ -9,11 +9,11 @@ function Cari(search_name, name)
 {
     indeks_cari= -1;
     result = [];
-    for(i=0;i<costumer.length;i++)
+    for(i=0;i<pelanggan.length;i++)
     {
-        if (costumer[i][name]== search_name)
+        if (pelanggan[i][name]== search_name)
         {
-            result.push(costumer[i]);
+            result.push(pelanggan[i]);
             indeks_dicari= i;
         }
     }
